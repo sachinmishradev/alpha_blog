f5992399d1842561ec44b3c6fe565f57420a986b
sachinmishradev <sachinmishra1614@gmail.com>
2018-06-05 20:22:22 +0530

    precpmpil

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
-//= require jquery
 //= require jquery_ujs
+//= require jquery
 //= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
