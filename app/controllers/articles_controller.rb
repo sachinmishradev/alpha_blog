0d05081cf012925b08e5e6e763b6526b61eb81a0
sachinmishradev <sachinmishra1614@gmail.com>
2018-06-05 17:41:57 +0530

    Styling installation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @article = Article.all
+  end
   def new
     @article = Article.new
   end
@@ -22,9 +26,33 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "The article was updated successfully"
+      redirect_to article_path(@article)
+
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article deleted successfully"
+    redirect_to articles_path
+  end
+
+
   private
     def article_params
       params.require(:article).permit(:title, :description)
     end
+
 
 end
