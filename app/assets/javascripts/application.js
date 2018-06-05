c618c47ad6b0bfee6871841a60aa6784a943961e
sachinmishradev <sachinmishra1614@gmail.com>
2018-06-05 18:06:30 +0530

    previous method online working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
-//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
